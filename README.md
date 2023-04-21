0c5e849b1818fac9ef2186fbe3f6209aa3409fd5
Tania Concil <99073493+Fragolaaa@users.noreply.github.com>
2023-04-21 12:47:58 +0200

    chkSession added

--- README.md
+++ README.md
@@ -4,4 +4,4 @@ template bootstrap: https://themewagon.com/themes/free-bootstrap-ecommerce-templ
 
 https://octopus.do/uu5kne10y18
 
-md5 password_cognome_nome
+md5 password_email
--- login_chk.php
+++ login_chk.php
@@ -1,6 +1,23 @@
 <?php
 include "\database\connection.php";
 
+$pwd = md5($_POST["password"]+"_"+$_POST["email"]);
 
+$stmt = $conn->prepare("SELECT * FROM users WHERE username = ? AND password = ?");
+$stmt->bind_param("ss", $_POST["username"], $pwd);
+$stmt->execute();
+$result = $stmt->get_result();
+
+$stmt->close();
+
+if ($result->num_rows > 0) {
+    $row = $result->fetch_assoc();
+    $_SESSION["username"] = $_POST["username"];
+    $_SESSION["userID"] = $row["ID"];
+    header("location: index.php?");
+} else {
+    header("location: login_page.php?err=Credenziali errate!&oldEmail=" . $_POST["username"]);
+}
+$conn->close();
 
 ?>
