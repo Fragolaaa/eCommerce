47b7a9c5b47a0f6c7d3eef9a9e37a937eb36eca0
Tania Concil <99073493+Fragolaaa@users.noreply.github.com>
2023-04-27 22:32:59 +0200

    controllo login

--- login_chk.php
+++ login_chk.php
@@ -1,23 +1,39 @@
 <?php
-include "\database\connection.php";
+include("database/connection.php");
 
-$pwd = md5($_POST["password"]+"_"+$_POST["email"]);
+session_start();
 
-$stmt = $conn->prepare("SELECT * FROM users WHERE username = ? AND password = ?");
-$stmt->bind_param("ss", $_POST["username"], $pwd);
-$stmt->execute();
-$result = $stmt->get_result();
+$email = $_POST['email'];
+$password = md5($_POST['password']);
 
-$stmt->close();
+$query = $conn->prepare("SELECT Id, E-mail, Password FROM users WHERE E-mail = ? AND Password = ?");
+$query->bind_param('sss', $email, $password);
+$query->execute();
+$result = $query->get_result();
 
 if ($result->num_rows > 0) {
-    $row = $result->fetch_assoc();
-    $_SESSION["username"] = $_POST["username"];
-    $_SESSION["userID"] = $row["ID"];
-    header("location: index.php?");
-} else {
-    header("location: login_page.php?err=Credenziali errate!&oldEmail=" . $_POST["username"]);
-}
-$conn->close();
+  $row = $result->fetch_assoc();
+  //save session
+  $_SESSION['ID'] = $row['ID'];
+  $_SESSION['Username'] = $row['Username'];
 
-?>
+  //last user's cart
+  $sql = $conn->prepare("SELECT MAX(Id) FROM shopping_cart WHERE UserID = ?");
+  $sql->bind_param('i', $_SESSION['ID']);
+  $sql->execute();
+  $result = $sql->get_result();
+  $row = $result->fetch_assoc();
+  $_SESSION['CartID'] = $row['MAX(Id)'];
+
+  //select user's wishlist
+  $sql = $conn->prepare("SELECT Id FROM wishlist WHERE UserID = ?");
+  $sql->bind_param('i', $_SESSION['ID']);
+  $sql->execute();
+  $result = $sql->get_result();
+  $row = $result->fetch_assoc();
+  $_SESSION['WishListID'] = $row['Id'];
+
+  header("location:index.php?msg=Logged in&type=success");
+} else {
+  header("location:login.php?msg=Incorrect email or password&type=danger");
+}
