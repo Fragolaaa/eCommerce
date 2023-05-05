51479bc5bea16891f155dd7b33b4829178ece103
Tania Concil <99073493+Fragolaaa@users.noreply.github.com>
2023-05-05 10:45:32 +0200

    Update connection.php

--- database/connection.php
+++ database/connection.php
@@ -2,7 +2,7 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "5ai_concil_dbecommerce";
+$dbname = "my_conciltania";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
